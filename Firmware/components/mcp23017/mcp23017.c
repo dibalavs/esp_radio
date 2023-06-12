5b4e8132457184c64abffe7e0b3b7a5975111be4
Vasily Dybala <dibalavs@yandex.ru>
2023-06-12 21:10:33 +0300

    mcp23017: Use native i2c API instead of custom bus impl.

--- Firmware/components/mcp23017/mcp23017.c
+++ Firmware/components/mcp23017/mcp23017.c
@@ -18,6 +18,8 @@
 
 static const char *TAG = "mcp23017";
 
+#define I2C_NO I2C_NUM_0    //  I2C port number for master dev
+
 #define MCP23017_CHECK(a, str, ret) if(!(a)) { \
         ESP_LOGE(TAG,"%s:%d (%s):%s", __FILE__, __LINE__, __FUNCTION__, str); \
         return (ret); \
@@ -74,11 +76,49 @@ typedef struct {
     uint16_t intEnabledPins;//pin of interrupt
 } mcp23017_dev_t;
 
+static uint8_t i2caddr;
+
+static esp_err_t my_i2c_bus_write_byte(i2c_bus_device_handle_t dev_handle, uint8_t mem_address, uint8_t data)
+{
+    (void)dev_handle;
+
+    esp_err_t ret=0;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    ESP_ERROR_CHECK(i2c_master_start(cmd));
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (i2caddr<<1) | WRITE_BIT , ACK_CHECK_EN));
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, mem_address, ACK_VAL));
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, data, ACK_VAL));
+    ESP_ERROR_CHECK(i2c_master_stop(cmd));
+    ret = i2c_master_cmd_begin(I2C_NO, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return ret;
+}
+
+static esp_err_t my_i2c_bus_read_byte(i2c_bus_device_handle_t dev_handle, uint8_t mem_address, uint8_t *data)
+{
+    (void)dev_handle;
+    esp_err_t ret=0;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    ESP_ERROR_CHECK(i2c_master_start(cmd));								// Send i2c start on bus
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (i2caddr<<1) | WRITE_BIT, ACK_CHECK_EN ));
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, mem_address, ACK_VAL));         // Send address to start read from
+    ESP_ERROR_CHECK(i2c_master_start(cmd));							    // Repeated start
+    ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (i2caddr<<1) | READ_BIT, ACK_CHECK_EN));
+    ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data, NACK_VAL));
+    ESP_ERROR_CHECK(i2c_master_stop(cmd));
+    ret = i2c_master_cmd_begin(I2C_NO, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
 mcp23017_handle_t mcp23017_create(i2c_bus_handle_t bus, uint8_t dev_addr)
 {
-    if (bus == NULL) {
-        return NULL;
-    }
+    i2caddr = dev_addr;
+    // if (bus == NULL) {
+    //     return NULL;
+    // }
 
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) calloc(1, sizeof(mcp23017_dev_t));
 
@@ -86,12 +126,12 @@ mcp23017_handle_t mcp23017_create(i2c_bus_handle_t bus, uint8_t dev_addr)
         return NULL;
     }
 
-    p_device->i2c_dev = i2c_bus_device_create(bus, dev_addr, 0);
+    // p_device->i2c_dev = i2c_bus_device_create(bus, dev_addr, 0);
 
-    if (p_device->i2c_dev == NULL) {
-        free(p_device);
-        return NULL;
-    }
+    // if (p_device->i2c_dev == NULL) {
+    //     free(p_device);
+    //     return NULL;
+    // }
 
     p_device->dev_addr = dev_addr;
     return (mcp23017_handle_t)p_device;
@@ -101,7 +141,7 @@ esp_err_t mcp23017_delete(mcp23017_handle_t *p_dev)
 {
     MCP23017_CHECK(p_dev != NULL && *p_dev != NULL, "invalid arg", ESP_ERR_INVALID_ARG)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *)(*p_dev);
-    i2c_bus_device_delete(&p_device->i2c_dev);
+   // i2c_bus_device_delete(&p_device->i2c_dev);
     free(p_device);
     *p_dev = NULL;
     return ESP_OK;
@@ -115,7 +155,7 @@ esp_err_t mcp23017_write(mcp23017_handle_t dev, uint8_t reg_start_addr,
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
     for (size_t i = 0; i < reg_num; i++) {
-        ret = i2c_bus_write_byte(p_device->i2c_dev, reg_start_addr + i, data_buf[i]);
+        ret = my_i2c_bus_write_byte(p_device->i2c_dev, reg_start_addr + i, data_buf[i]);
 
         if (ret != ESP_OK) {
             return ret;
@@ -133,7 +173,7 @@ esp_err_t mcp23017_read(mcp23017_handle_t dev, uint8_t reg_start_addr,
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
     for (size_t i = 0; i < reg_num; i++) {
-        ret = i2c_bus_read_byte(p_device->i2c_dev, reg_start_addr + i, &data_buf[i]);
+        ret = my_i2c_bus_read_byte(p_device->i2c_dev, reg_start_addr + i, &data_buf[i]);
 
         if (ret != ESP_OK) {
             return ret;
@@ -147,11 +187,11 @@ esp_err_t mcp23017_set_pullup(mcp23017_handle_t dev, uint16_t pins)
 {
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
     esp_err_t ret = ESP_FAIL;
-    ret = i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_GPPUA, MCP23017_PORT_A_BYTE(pins));
+    ret = my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_GPPUA, MCP23017_PORT_A_BYTE(pins));
     if (ret != ESP_OK)
         return ret;
 
-    ret = i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_GPPUB, MCP23017_PORT_B_BYTE(pins));
+    ret = my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_GPPUB, MCP23017_PORT_B_BYTE(pins));
 
     return ret;
 }
@@ -160,11 +200,11 @@ esp_err_t mcp23017_set_input_polarity(mcp23017_handle_t dev, uint16_t bits)
 {
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
     esp_err_t ret = ESP_FAIL;
-    ret = i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_IPOLA, MCP23017_PORT_A_BYTE(bits));
+    ret = my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_IPOLA, MCP23017_PORT_A_BYTE(bits));
     if (ret != ESP_OK)
         return ret;
 
-    ret = i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_IPOLB, MCP23017_PORT_B_BYTE(bits));
+    ret = my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_IPOLB, MCP23017_PORT_B_BYTE(bits));
 
     return ret;
 }
@@ -241,7 +281,7 @@ esp_err_t mcp23017_set_interrupt_polarity(mcp23017_handle_t dev,
         setIOCON[1] = *ioCONValue & ~MCP23017_IOCON_INTPOL;
     }
 
-    return i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
+    return my_i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
 }
 
 esp_err_t mcp23017_set_seque_mode(mcp23017_handle_t dev, uint8_t isSeque)
@@ -265,7 +305,7 @@ esp_err_t mcp23017_set_seque_mode(mcp23017_handle_t dev, uint8_t isSeque)
         setIOCON[1] = *ioCONValue & ~MCP23017_IOCON_SEQOP;
     }
 
-    return i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
+    return my_i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
 }
 
 esp_err_t mcp23017_mirror_interrupt(mcp23017_handle_t dev, uint8_t mirror,
@@ -297,7 +337,7 @@ esp_err_t mcp23017_mirror_interrupt(mcp23017_handle_t dev, uint8_t mirror,
         setIOCON[1] = *ioCONValue & ~MCP23017_IOCON_MIRROR;
     }
 
-    return i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
+    return my_i2c_bus_write_byte(p_device->i2c_dev, setIOCON[0], setIOCON[1]);
 }
 
 esp_err_t mcp23017_set_io_dir(mcp23017_handle_t dev, uint8_t value,
@@ -306,7 +346,7 @@ esp_err_t mcp23017_set_io_dir(mcp23017_handle_t dev, uint8_t value,
     MCP23017_CHECK(dev != NULL, "invalid arg", ESP_ERR_INVALID_ARG)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
-    return i2c_bus_write_byte(p_device->i2c_dev,
+    return my_i2c_bus_write_byte(p_device->i2c_dev,
                                (gpio == MCP23017_GPIOA) ?
                                MCP23017_REG_IODIRA : MCP23017_REG_IODIRB, value);
 }
@@ -317,7 +357,7 @@ esp_err_t mcp23017_write_io(mcp23017_handle_t dev, uint8_t value,
     MCP23017_CHECK(dev != NULL, "invalid arg", ESP_ERR_INVALID_ARG)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
-    return i2c_bus_write_byte(p_device->i2c_dev,
+    return my_i2c_bus_write_byte(p_device->i2c_dev,
                                (gpio == MCP23017_GPIOA) ? MCP23017_REG_GPIOA : MCP23017_REG_GPIOB,
                                value);
 }
@@ -328,7 +368,7 @@ uint8_t mcp23017_read_io(mcp23017_handle_t dev, mcp23017_gpio_port_t gpio)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
     uint8_t data = 0;
-    i2c_bus_read_byte(p_device->i2c_dev,
+    my_i2c_bus_read_byte(p_device->i2c_dev,
                       (gpio == MCP23017_GPIOA) ? MCP23017_REG_GPIOA : MCP23017_REG_GPIOB,
                       &data);
     return data;
@@ -340,7 +380,7 @@ uint8_t mcp23017_read_intcap(mcp23017_handle_t dev, mcp23017_gpio_port_t gpio)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
 
     uint8_t data = 0;
-    i2c_bus_read_byte(p_device->i2c_dev,
+    my_i2c_bus_read_byte(p_device->i2c_dev,
                       (gpio == MCP23017_GPIOA) ? MCP23017_REG_INTCAPA : MCP23017_REG_INTCAPB,
                       &data);
     return data;
@@ -391,9 +431,9 @@ esp_err_t mcp23017_check_present(mcp23017_handle_t dev)
     mcp23017_dev_t *p_device = (mcp23017_dev_t *) dev;
     uint8_t lastregValue = 0x00;
     uint8_t regValue = 0x00;
-    i2c_bus_read_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, &lastregValue);
-    i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, 0xAA);
-    i2c_bus_read_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, &regValue);
-    i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, lastregValue);
+    my_i2c_bus_read_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, &lastregValue);
+    my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, 0xAA);
+    my_i2c_bus_read_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, &regValue);
+    my_i2c_bus_write_byte(p_device->i2c_dev, MCP23017_REG_INTCONA, lastregValue);
     return (regValue == 0xAA) ? ESP_OK : ESP_FAIL;
 }
